b9c3d475dc7e08523806092094e1bd1bf2f16b68
Nada_Elhor <95687971+NadaElhor20@users.noreply.github.com>
2022-02-21 22:24:49 +0200

    Delete Modules/visits directory

--- Modules/visits/visitModel.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-const visitSchema = require('./visitSchema');
-const Visit = mongoose.model('Visit', visitSchema,'visitsCollection');
-
-module.exports = Visit;
